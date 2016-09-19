4aef12f620b7f61d157af2b76a300af191cea33d
wakamesoba98 <wakamesoba98@gmail.com>
2016-09-19 14:53:43 +0900

    Use readfile

--- ipbadge.php
+++ ipbadge.php
@@ -1,4 +1,5 @@
 <?php
 header("Content-type: image/png");
 $ipversion = strpos($_SERVER["REMOTE_ADDR"], ":") === false ? 4 : 6;
-echo file_get_contents(__DIR__ . "/ipv{$ipversion}.png");
+readfile(__DIR__ . "/ipv{$ipversion}.png");
+?>
